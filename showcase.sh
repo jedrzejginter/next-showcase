2acb9ed262d2f4196f20a84428c1367d6ef320bc
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2020-10-27 11:07:37 +0100

    Add showcase to package

--- showcase.sh
+++ showcase.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
+DIR=$(cd -P $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 if [ "$1" == "watch" ]; then
-  npx onchange -v 'src/**/*.stories.*' -- node ./cli.js
+  npx onchange -v 'src/**/*.stories.*' -- node ${DIR}/cli.js
 else
-  node ./cli.js
+  node ${DIR}/cli.js
 fi
 
