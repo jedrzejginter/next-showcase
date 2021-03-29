de5634cc75e02b9f917dc576feae262e5e094a58
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2021-03-29 10:32:53 +0200

    chore: bump toolkit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,6 @@
-const eslintConfig = require('@ginterdev/toolkit/eslint');
-
 module.exports = {
-  ...eslintConfig,
-  extends: eslintConfig.extends.filter(
-    (ext) => !/prettier\/(react|@typescript-eslint)/.test(ext),
-  ),
+  extends: require.resolve('@ginterdev/toolkit/eslint'),
   rules: {
-    ...eslintConfig.rules,
     'import/no-extraneous-dependencies': [
       'error',
       { devDependencies: ['**/.*rc.js'] },
